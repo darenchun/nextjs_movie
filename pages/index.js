2deac07a5a2cbbce78bbfeee2fabf62c7c83c0ca
darenchun <darenchun@naver.com>
2023-01-15 17:52:56 +0900

    practice loop component in jsx

--- pages/index.js
+++ pages/index.js
@@ -6,30 +6,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Welcome Next App</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <h1 className={styles.title}>
-          Read <Link href={"/posts/first_post"}>go to this page!!</Link>
+          <Link href={"/posts/first_post"}>go to this page!!</Link>
         </h1>
-
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
-
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
@@ -37,11 +30,9 @@ export default function Home() {
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
-
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
+            className={styles.card}          >
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -49,7 +40,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
       <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -111,6 +101,7 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
+
     </div>
   )
 }
--- pages/posts/first_post.js
+++ pages/posts/first_post.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
-
+import React from 'react';
 const MyImage = () => (
     <Image
         src="/images/screenshot_01.png"
@@ -9,17 +9,23 @@ const MyImage = () => (
         alt="Your name" />
 );
 
+const numbering_arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 
 
 export default function FirstPost() {
+    const [likes, setLikes] = React.useState(0);
+    const handleClick = () =>{setLikes(likes+1)};
+
+    
     return (
         <>
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to Home</Link>
             </h2>
-            <img src="/images/screenshot_01.png" alt="your name" />
-            <MyImage/>
+            <br />
+            <button onClick={handleClick}>Likes : ({likes})</button>
         </>
     );
 
