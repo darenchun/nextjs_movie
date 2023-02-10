156f80aac486859ef085651a8b7cbbf37d937f39
JDK <36753565+darenchun@users.noreply.github.com>
2023-02-10 07:20:33 +0000

    test commit

--- pages/posts/first_post.js
+++ pages/posts/first_post.js
@@ -15,14 +15,15 @@ const numbering_arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function FirstPost() {
     const [likes, setLikes] = React.useState(0);
-    const handleClick = () =>{setLikes(likes+1)};
-
-    
+    const handleClick = () => { setLikes(likes + 1) };
     return (
         <>
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to Home</Link>
+
+
+
             </h2>
             <br />
             <button onClick={handleClick}>Likes : ({likes})</button>
